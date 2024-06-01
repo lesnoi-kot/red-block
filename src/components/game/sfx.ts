ee1f0346247df3591252b3f15fcef64b205269be
yuko <yuriii.kovalyov@gmail.com>
2024-06-01 23:53:58 +0300

    meta tags

--- src/components/game/sfx.ts
+++ src/components/game/sfx.ts
@@ -2,8 +2,8 @@ export const winAudio = new Audio("/elevbell1.wav");
 export const resetAudio = new Audio("/button4.wav");
 export const startAudio = new Audio("/lever4.wav");
 
-winAudio.volume = 0.2;
-startAudio.volume = 0.2;
+winAudio.volume = 0.4;
+startAudio.volume = 0.4;
 resetAudio.volume = 0.2;
 
 export function playAudio(audio: HTMLAudioElement) {
